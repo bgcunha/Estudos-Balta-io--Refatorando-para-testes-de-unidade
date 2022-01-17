bc28bab8f2d5d620612f815d9531f21a945dae84
Bruno Gomes <brunnu.cunha@gmail.com>
2022-01-17 15:07:40 -0300

    faltando um teste para fficar verde

--- good/Store/Store.Tests/Handlres/OrderHandlerTests.cs
+++ good/Store/Store.Tests/Handlres/OrderHandlerTests.cs
@@ -29,32 +29,93 @@ public class OrderHandlerTests
     [TestCategory("Handlers")]
     public void Dado_um_cliente_inexistente_o_pedido_nao_deve_ser_gerado()
     {
-        // TODO: Implementar
-        Assert.Fail();
+        var command = new CreateOrderCommand();
+        command.Customer = "";
+        command.ZipCode = "13411080";
+        command.PromoCode = "12345678";
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+
+        var handler = new OrderHandler(
+             null,
+             _deliveryFeeRepository,
+             _discountRepository,
+             _productRepository,
+             _orderRepository);
+
+        handler.Handle(command);
+        Assert.AreEqual(handler.Valid, false);
     }
 
     [TestMethod]
     [TestCategory("Handlers")]
     public void Dado_um_cep_invalido_o_pedido_deve_ser_gerado_normalmente()
     {
-        // TODO: Implementar
-        Assert.Fail();
+        var command = new CreateOrderCommand();
+        command.Customer = "12345678";
+        command.ZipCode = "12345678";
+        command.PromoCode = "12345678";
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+
+
+        var handler = new OrderHandler(
+            _customerRepository,
+            _deliveryFeeRepository,
+            _discountRepository,
+            _productRepository,
+            _orderRepository);
+
+        handler.Handle(command);
+        Assert.AreEqual(handler.Valid, true);
     }
 
     [TestMethod]
     [TestCategory("Handlers")]
     public void Dado_um_promocode_inexistente_o_pedido_deve_ser_gerado_normalmente()
     {
-        // TODO: Implementar
-        Assert.Fail();
+        var command = new CreateOrderCommand();
+        command.Customer = "12345678";
+        command.ZipCode = "13411080";
+        command.PromoCode = "0000";
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+
+        var handler = new OrderHandler(
+            _customerRepository,
+            _deliveryFeeRepository,
+            _discountRepository,
+            _productRepository,
+            _orderRepository);
+
+        handler.Handle(command);
+        Assert.AreEqual(handler.Valid, true);
     }
 
     [TestMethod]
     [TestCategory("Handlers")]
     public void Dado_um_pedido_sem_itens_o_mesmo_nao_deve_ser_gerado()
     {
-        // TODO: Implementar
-        Assert.Fail();
+        var command = new CreateOrderCommand();
+        command.Customer = "12345678";
+        command.ZipCode = "13411080";
+        command.PromoCode = "12345678";
+        // command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+        // command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+
+        command.Validate();
+
+        // var handler = new OrderHandler(
+        //     _customerRepository,
+        //     _deliveryFeeRepository,
+        //     _discountRepository,
+        //     _productRepository,
+        //     _orderRepository);
+
+        // handler.Handle(command);
+        // Assert.AreEqual(handler.Valid, false);
+
+        Assert.AreEqual(command.Valid, false);
     }
 
     [TestMethod]
